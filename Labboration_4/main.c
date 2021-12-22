bbb0d533532f5b239854d153dc765ef762b1de17
Per-Erik Ljung <pererikljung@gmail.com>
2021-12-22 21:42:10 +0100

    Början på 4

--- Labboration_4/main.c
+++ Labboration_4/main.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-
-
-
+#include <stdlib.h>
 int main(){
-	
-	
-	
-	
-	
-	
-	
-	
+
+int choice;
+
+printf("Välkommen till Trafikverkets databas för fordon \n\n Nedan kommer alternativ som går att göra i programmet \n1. Lägg till Fordon \n2. Ta bort ett Fordon \n3. Sortering efter bilmärke \n4. Skriv ut information om Fordon\n5. Skriv ut hela fordonsregistret \n6. Avsluta programmet");
+scanf("%d", &choice);
+
+
+
+
 }
-
-
-
